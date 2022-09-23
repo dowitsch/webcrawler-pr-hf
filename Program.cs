fd919bd940c35ecfa1037fc0684679f91dfdeab5
Dowitsch <fexu@whatwedo.ch>
2022-09-23 12:15:39 +0200

    feat: simple web crawler

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
 ﻿namespace HelloWorld
 {
+
+    using CsvHelper;
+    using HtmlAgilityPack;
+    using System.IO;
+    using System.Collections.Generic;
+    using System.Globalization;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://en.wikipedia.org/wiki/Greece");
+            var HeaderNames = doc.DocumentNode.SelectNodes("//span[@class='toctext']");
+            var titles = new List<Row>();
+            foreach (var item in HeaderNames)
+            {
+            titles.Add(new Row { Title = item.InnerText});
+            }
+            using (var writer = new StreamWriter("./output/example.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+            csv.WriteRecords(titles);
+            }
         }
+    }
+
+
+    public class Row
+    {
+        public string Title {get; set;}
     }
 }
